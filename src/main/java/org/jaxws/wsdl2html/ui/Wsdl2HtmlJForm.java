267cd44c71c0fd8e8a7b0aae556294c57d268b98
mohammed barqawi <mohammed.barqawi@gmail.com>
2018-04-29 21:24:22 +0300

    UI update

--- src/main/java/org/jaxws/wsdl2html/ui/Wsdl2HtmlJForm.java
+++ src/main/java/org/jaxws/wsdl2html/ui/Wsdl2HtmlJForm.java
@@ -3,6 +3,7 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JTextField;
+import javax.swing.filechooser.FileNameExtensionFilter;
 import javax.swing.filechooser.FileSystemView;
 
 import org.apache.commons.io.FileUtils;
@@ -18,22 +19,30 @@ import javax.swing.JOptionPane;
 
 import java.awt.event.ActionListener;
 import java.io.File;
+import java.io.FileFilter;
 import java.io.IOException;
 import java.awt.event.ActionEvent;
 import java.awt.FlowLayout;
 import java.awt.GridLayout;
 import javax.swing.BoxLayout;
 import java.awt.BorderLayout;
+import java.awt.Desktop;
+
 import javax.swing.GroupLayout;
 import javax.swing.GroupLayout.Alignment;
 import javax.swing.LayoutStyle.ComponentPlacement;
 import javax.swing.JTextPane;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.awt.Window.Type;
 //import net.miginfocom.swing.MigLayout;
 
 public class Wsdl2HtmlJForm {
 
 	private JFrame frmWsdlhtml;
 	private JTextField textWSDLFilePathField;
+	private JTextField textFieldForOutput;
+	String Wrong_output_folder = "This Is not valid output folder ";
 
 	/**
 	 * Launch the application.
@@ -63,8 +72,15 @@ public class Wsdl2HtmlJForm {
 	 */
 	private void initialize() {
 		frmWsdlhtml = new JFrame();
+		frmWsdlhtml.setType(Type.UTILITY);
+		frmWsdlhtml.addWindowListener(new WindowAdapter() {
+			@Override
+			public void windowOpened(WindowEvent arg0) {
+				textFieldForOutput.setText( new File("output").getAbsolutePath());
+			}
+		});
 		frmWsdlhtml.setTitle("WSDL2HTML");
-		frmWsdlhtml.setBounds(100, 100, 491, 608);
+		frmWsdlhtml.setBounds(100, 100, 520, 608);
 		frmWsdlhtml.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		final JTextPane textPane1 = new JTextPane();
 		JLabel lblEnterTheWsdl = new JLabel("Enter the WSDL Local Path ");
@@ -76,7 +92,7 @@ public class Wsdl2HtmlJForm {
 		btnGenrateDocunantion.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
 				
-				String tempWSDl="C:\\Users\\mohammed\\git\\wsdl2html\\resources\\wsdl\\BankCustomerService.wsdl";
+				
 				String wsdlUrl= textWSDLFilePathField.getText();
 				
 				File f = new File(wsdlUrl);
@@ -88,7 +104,16 @@ public class Wsdl2HtmlJForm {
 				
 				try {
 					String html = org.jaxws.wsdl2html.service.Wsdl2Html.generateHtml(wsdlUrl);
-					File htmlDir = new File("output");
+					
+					File htmlDir = new File(textFieldForOutput.getText());
+					File file = new File(textFieldForOutput.getText());
+					if(!file.isDirectory())
+					{
+						
+						JOptionPane.showMessageDialog(frmWsdlhtml, Wrong_output_folder);
+						return;
+					}
+					
 					String reportFileName=f.getName()+".html";
 					File fileReport = new File(htmlDir, reportFileName);
 					FileUtils.writeStringToFile(fileReport, html);
@@ -109,11 +134,12 @@ public class Wsdl2HtmlJForm {
 		JButton btnBrowseWSDL = new JButton("Browse for WSDL file");
 		btnBrowseWSDL.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent arg0) {
+			
 				JFileChooser jfc = new JFileChooser(FileSystemView.getFileSystemView().getHomeDirectory());
+				jfc.addChoosableFileFilter(new FileNameExtensionFilter("wsdl","wsdl"));
 
 				int returnValue = jfc.showOpenDialog(null);
-				// int returnValue = jfc.showSaveDialog(null);
-
+			
 				if (returnValue == JFileChooser.APPROVE_OPTION) {
 					File selectedFile = jfc.getSelectedFile();
 					textWSDLFilePathField.setText(selectedFile.getAbsolutePath());
@@ -121,46 +147,80 @@ public class Wsdl2HtmlJForm {
 			}
 		});
 		
+		textFieldForOutput = new JTextField();
+		textFieldForOutput.setColumns(10);
+		
+		JButton btnOpenOutput = new JButton("Open Output");
+		btnOpenOutput.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				try {
+					
+					File file = new File(textFieldForOutput.getText());
+					if(!file.isDirectory())
+					{
+						JOptionPane.showMessageDialog(frmWsdlhtml,Wrong_output_folder);
+						return;
+					}
+					Desktop.getDesktop().open(file);
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		});
+		
+		JLabel lblOutputPath = new JLabel("Output path ");
+		
 		
 		GroupLayout groupLayout = new GroupLayout(frmWsdlhtml.getContentPane());
 		groupLayout.setHorizontalGroup(
 			groupLayout.createParallelGroup(Alignment.LEADING)
 				.addGroup(groupLayout.createSequentialGroup()
-					.addGap(161)
+					.addGap(18)
 					.addComponent(btnBrowseWSDL)
-					.addContainerGap(181, Short.MAX_VALUE))
-				.addGroup(groupLayout.createSequentialGroup()
-					.addGap(151)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(btnOpenOutput)
+					.addGap(15)
 					.addComponent(btnGenrateDocunantion)
-					.addContainerGap(171, Short.MAX_VALUE))
+					.addContainerGap(84, Short.MAX_VALUE))
+				.addGroup(Alignment.TRAILING, groupLayout.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(groupLayout.createParallelGroup(Alignment.TRAILING)
+						.addComponent(textWSDLFilePathField, GroupLayout.DEFAULT_SIZE, 470, Short.MAX_VALUE)
+						.addComponent(textPane1, GroupLayout.PREFERRED_SIZE, 454, GroupLayout.PREFERRED_SIZE))
+					.addGap(24))
 				.addGroup(groupLayout.createSequentialGroup()
-					.addGap(132)
-					.addComponent(lblEnterTheWsdl, GroupLayout.DEFAULT_SIZE, 187, Short.MAX_VALUE)
-					.addGap(156))
+					.addContainerGap()
+					.addComponent(textFieldForOutput, GroupLayout.DEFAULT_SIZE, 470, Short.MAX_VALUE)
+					.addGap(24))
 				.addGroup(groupLayout.createSequentialGroup()
-					.addGap(28)
-					.addGroup(groupLayout.createParallelGroup(Alignment.LEADING)
-						.addGroup(groupLayout.createSequentialGroup()
-							.addComponent(textPane1, GroupLayout.DEFAULT_SIZE, 414, Short.MAX_VALUE)
-							.addContainerGap())
-						.addGroup(groupLayout.createSequentialGroup()
-							.addComponent(textWSDLFilePathField, GroupLayout.DEFAULT_SIZE, 423, Short.MAX_VALUE)
-							.addGap(24))))
+					.addContainerGap()
+					.addComponent(lblOutputPath, GroupLayout.PREFERRED_SIZE, 131, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(363, Short.MAX_VALUE))
+				.addGroup(groupLayout.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(lblEnterTheWsdl)
+					.addContainerGap(363, Short.MAX_VALUE))
 		);
 		groupLayout.setVerticalGroup(
 			groupLayout.createParallelGroup(Alignment.LEADING)
 				.addGroup(groupLayout.createSequentialGroup()
-					.addGap(24)
-					.addComponent(lblEnterTheWsdl, GroupLayout.PREFERRED_SIZE, 57, GroupLayout.PREFERRED_SIZE)
-					.addGap(41)
+					.addGap(56)
+					.addComponent(lblEnterTheWsdl, GroupLayout.PREFERRED_SIZE, 30, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.RELATED)
 					.addComponent(textWSDLFilePathField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
-					.addGap(18)
-					.addComponent(btnBrowseWSDL, GroupLayout.PREFERRED_SIZE, 51, GroupLayout.PREFERRED_SIZE)
-					.addGap(18)
-					.addComponent(btnGenrateDocunantion, GroupLayout.PREFERRED_SIZE, 39, GroupLayout.PREFERRED_SIZE)
-					.addGap(18)
+					.addGap(22)
+					.addComponent(lblOutputPath, GroupLayout.PREFERRED_SIZE, 30, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(textFieldForOutput, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+					.addGap(45)
+					.addGroup(groupLayout.createParallelGroup(Alignment.BASELINE)
+						.addComponent(btnGenrateDocunantion)
+						.addComponent(btnBrowseWSDL)
+						.addComponent(btnOpenOutput))
+					.addGap(30)
 					.addComponent(textPane1, GroupLayout.PREFERRED_SIZE, 267, GroupLayout.PREFERRED_SIZE)
-					.addContainerGap(16, Short.MAX_VALUE))
+					.addContainerGap(14, Short.MAX_VALUE))
 		);
 		frmWsdlhtml.getContentPane().setLayout(groupLayout);
 	}
